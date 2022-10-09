1acaaaaa8ef5f82cd57785240ccfcf7dd4d2f6a7
Cristian Lopez <cristianlopezr.931@gmail.com>
2022-10-09 11:52:46 -0300

    Delete functionality fixed

--- src/hooks/useCalculate.js
+++ src/hooks/useCalculate.js
@@ -4,17 +4,32 @@ const initialState = {
 	result: '0',
 	operation: '',
 	action: '',
-	restart: false,
+	canRestartResultText: false,
 	lastNumberPicked: 0,
 	previousNumber: 0,
+	restartCalculator: false,
 };
 
 export const useCalculate = () => {
 	const [calculation, setCalculation] = useState(initialState);
 
-	const { result, operation, action, lastNumberPicked, previousNumber, restart } = calculation;
+	const {
+		result,
+		operation,
+		action,
+		lastNumberPicked,
+		previousNumber,
+		canRestartResultText,
+		restartCalculator,
+	} = calculation;
 
 	const onAddNumber = number => {
+		if (restartCalculator)
+			return setCalculation(() => ({
+				...initialState,
+				result: number,
+			}));
+
 		if (number === ',') {
 			if (result.includes('.')) return;
 
@@ -30,13 +45,14 @@ export const useCalculate = () => {
 				...currentCalc,
 				result: number,
 				lastNumberPicked: Number(number),
+				canRestartResultText: false,
 			}));
 
-		restart
+		canRestartResultText
 			? setCalculation(currentCalc => ({
 					...currentCalc,
 					result: number,
-					restart: false,
+					canRestartResultText: false,
 					lastNumberPicked: Number(number),
 			  }))
 			: setCalculation(currentCalc => ({
@@ -53,11 +69,22 @@ export const useCalculate = () => {
 		}
 
 		if (action === 'X') {
-			if (result.length === 1)
-				return setCalculation(currentCalc => ({
+			if (restartCalculator) {
+				setCalculation(() => ({
+					...initialState,
+					result: result,
+					restartCalculator: true,
+				}));
+				return;
+			}
+
+			if (result.length === 1) {
+				setCalculation(currentCalc => ({
 					...currentCalc,
 					result: initialState.result,
 				}));
+				return;
+			}
 
 			setCalculation(currentCalc => ({
 				...currentCalc,
@@ -71,7 +98,8 @@ export const useCalculate = () => {
 			previousNumber: Number(result),
 			operation: `${result} ${action}`,
 			action: action,
-			restart: true,
+			canRestartResultText: true,
+			restartCalculator: false,
 		}));
 	};
 
@@ -79,55 +107,13 @@ export const useCalculate = () => {
 		if (action === '+') {
 			setCalculation(currentCalc => ({
 				...currentCalc,
-				result: Number(previousNumber) + Number(lastNumberPicked),
+				result: (previousNumber + lastNumberPicked).toString(),
 				operation: `${previousNumber} ${action} ${lastNumberPicked} ${equal} `,
 				previousNumber: Number(previousNumber) + Number(lastNumberPicked),
+				restartCalculator: true,
 			}));
 		}
 	};
-
-	/* const onClickAction = action => {
-		if (action === 'AC') {
-			setCalculation(initialState);
-			return;
-		}
-
-		if (action === '+') {
-			setCalculation(currentCalc => ({
-				...currentCalc,
-				operation: `${currentCalc.result} ${action}`,
-				restart: true,
-			}));
-			return;
-		}
-
-		if (action === '=') {
-			if (calculation.action === '+') {
-				setCalculation(currentCalc => ({
-					...currentCalc,
-					result: Number(currentCalc.result) + lastNumberPicked,
-					operation: ` ${currentCalc.result} ${action}`,
-					lastNumberPicked: Number(currentCalc.result),
-				}));
-				return;
-			}
-			setCalculation(currentCalc => ({
-				...currentCalc,
-				operation: `${currentCalc.result}`,
-				action: action,
-			}));
-			return;
-		}
-
-		setCalculation(currentCalc => ({
-			...currentCalc,
-			operation: `${currentCalc.result} ${action}`,
-			action: action,
-			restart: true,
-			lastNumberPicked: Number(currentCalc.result),
-		}));
-	};
-	console.log(lastNumberPicked); */
 
 	return {
 		result,
